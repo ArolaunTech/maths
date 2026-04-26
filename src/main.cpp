9bfc004097e9bf1a36c538e5ca24dccafaaae927
ArolaunTech <aadityasenji@gmail.com>
2026-04-26 14:07:08 -0700

    Initial integer class

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "ui/terminal.h"
+#include "types/integer.h"
+
+int main() {
+	Integer a, b;
+
+	
+}
---
+++ src/types/integer.h
@@ -0,0 +1,12 @@
+#include <vector>
+#include <cstdint>
+
+#pragma once
+
+class Integer {
+private:
+	bool negative;
+
+	std::vector<std::uint32_t> digits;
+public:
+};
